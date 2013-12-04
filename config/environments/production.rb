8ff5a2cb23077d823800cb49f9cde0489c0e75ee
apecherin <artempecherin@gmail.com>
2013-12-04 11:43:11 +0400

    Some

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Reps::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -77,4 +77,6 @@ Reps::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { :host => 'hidden-island-8670.herokuapp.com' }
 end
